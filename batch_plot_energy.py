93021f2bdb1c6cc9c1d3166da858de47ebdfbcd2
chen <chenlingxiao0617@163.com>
2024-08-20 10:31:16 +0800

    批量能量图

---
+++ batch_plot_energy.py
@@ -0,0 +1,102 @@
+import meta
+from meta import results, utils, windows, plot2d, models, toolbars
+import os
+
+from metaLiteral import CurveOpts
+
+def find_dyna_files(folder_path):
+    d3plot_files = []  
+
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file == "d3plot":
+                file_path = os.path.join(root, file)
+                d3plot_files.append(file_path)  
+
+    return d3plot_files
+
+def dirs_in_path(path:str):
+    contents = os.listdir(path) # all files + directors in total
+    subdirs = [f for f in contents if os.path.isdir(os.path.join(path, f))] # pick directors in total
+    return subdirs
+
+def new_script():
+    utils.MetaCommand('window clearcreate 3d keepses')
+    utils.MetaCommand('report clear all')
+    utils.MetaCommand('spreadsheet clearall')
+    utils.MetaCommand('spreadsheet windows deleteall')
+
+
+def plot_energy(d3plot):
+    # create window
+    window_name = "energy"
+    windows.Create2DPlotWindow(window_name)
+    
+    page_id = 0
+    win = windows.Window(window_name,page_id)
+    win.set_plots_settings({'legend':1})
+
+    # path
+    dir = os.path.dirname(d3plot)
+    glstat = os.path.join(dir,"*")
+    output = os.path.join(dir,"energy.jpg")
+
+    # create curve
+    plot_id = 1
+    type = "glstat-Global"
+    entities = ["1"]
+    variables = CurveOpts # enum is default a iterater, filter out some you don't want to plot
+    
+    new_curves = plot2d.LoadCurvesDyna(window_name, plot_id, glstat, type, entities, variables)
+    
+    # set curve width
+    all_curves = win.get_curves()
+    for cur in all_curves:
+        cur.set_line_width(2)
+    
+    
+    # set axis title
+    yaxis = plot2d.PlotAxis(id=0, type="yaxis", plot_id=0, window_name=window_name, page_id=0)
+    title = "Energy"
+    yaxis.set_title(title)
+    xaxis = plot2d.PlotAxis(id=0, type="xaxis", plot_id=0, window_name=window_name, page_id=0)
+    title = "Time"
+    xaxis.set_title(title)
+    
+    # output window
+    utils.MetaCommand(f'write jpeg "{output}" 85')
+
+def main():
+    os.system('cls')
+    
+    path = toolbars.TextboxGetValue('report', 'root')
+    subdirs = dirs_in_path(path)
+    
+    d3plot = []
+    for subdir in subdirs:
+        if subdir.startswith(toolbars.TextboxGetValue('report', 'filter')):
+            subdir_path = os.path.join(path,subdir)
+            d3plot.extend(find_dyna_files(subdir_path))
+    
+    for d in d3plot:
+        new_script()
+        plot_energy(d)
+
+               
+    
+    
+if __name__ == "__main__":
+    os.system('cls')
+    
+    path = r"Z:\cae_jobs\bus"
+    subdirs = dirs_in_path(path)
+    
+    d3plot = []
+    for subdir in subdirs:
+        if subdir.startswith("08"):
+            subdir_path = os.path.join(path,subdir)
+            d3plot.extend(find_dyna_files(subdir_path))
+            
+    new_script()
+    plot_energy(d3plot[0])
+                
