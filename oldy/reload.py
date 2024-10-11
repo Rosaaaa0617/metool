cfeb1da2eef0e1febb5d82176e3c5fdf82fab6c9
chen <chenlingxiao0617@163.com>
2024-10-11 14:44:57 +0800

    i have to say, the first is the best...

--- oldy/reload.py
+++ oldy/reload.py
@@ -1,22 +1,13 @@
 import meta,os,helpers
 from meta import results,utils,models,windows
 
-def read_geo(d3plot:str):
-    window_name = "MetaPost"
-    deck = "DYNA"    
-    models.LoadModel(window_name, d3plot, deck)
-
-def read_deform(d3plot:str,states:str='all'):
-    model_id = 0
-    deck = "DYNA"
-    data_scalar = 'Displacements'
-    results.LoadDeformations(model_id, d3plot, deck, states, data_scalar)
-
 def reload():
-    dir = helpers.get_current_model_dir()
-    d3plot = os.path.join(dir,'d3plot')
-    
-    read_deform(d3plot)
+    models = utils.get_models('all')
+    d3plot = models[0].name
+            
+    utils.MetaCommand('window active MetaPost')
+    utils.MetaCommand(f'read geom Dyna3d "{d3plot}"')
+    utils.MetaCommand(f'read dis Dyna3d "{d3plot}" all Displacements')
     
 if __name__ == "__main__":
     time = helpers.NewScript()
